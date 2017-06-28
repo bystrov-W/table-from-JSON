a6fcf50261206eb050cd0c023dd68e888d518c34
bystrov-W <bystrov.v.v@outlook.com>
2017-06-28 17:40:13 +0300

    Update 1[1].php

--- 1[1].php
+++ 1[1].php
@@ -1,40 +1,51 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Page Title</title>
+	<title>Вывод таблицы из JSON</title>
 </head>
 <body>
 
 <?php
 $text= file_get_contents("1.json");
 $json = json_decode($text);
-
-
-function objectToarray($json)
-   {
-       $array = (array)$json;
-       foreach($array as $key => &$field){
-           if(is_object($field))$field = $this->objectToarray($field);
-       }
-       return $array;
-   }
-
-
 //echo $text ;
 foreach ($json->people as $obj):
 ?>
-    Имя: <?=$obj->firstName?><br>
-    Фамилия: <?=$obj->lastName?><br>
-	Адрес: <?=$obj->address?><br>
-    Номер тел. : <?=$obj-> phoneNumber?><hr>
+
+
+<table width="30%" >
+<br>
+<th width="30%" >
+	Имя:<br>Фамилия: <br>Адрес:<br>Номер тел. :<br>
+</th> 
+
+<td>  
+	<?=$obj-> firstName?><br>
+	<?=$obj-> lastName?><br>
+	<?=$obj-> address?><br>
+	<?=$obj-> phoneNumber?>
+</td>
+   
+</table>
 	
 <?php endforeach; ?>
 
+<style>
+    table { 
+        border-spacing: 10;
+        border-collapse: collapse;
+		;
+	  }
 
-
-
-
-
+    table td, th {
+        border: 2px solid #ccc;
+        padding: 15px;
+  		 }
+    th {background: #4682B4;}
+    td {background: #eee;}
+	
+</style>
+	
 </body>
 </html>
 
